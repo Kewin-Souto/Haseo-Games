64caeba6ea2894162499c4b5505f7e2ac81354e3
Kewin Souto Barros <77506025+Kewin-Souto@users.noreply.github.com>
2021-02-27 21:48:54 -0300

    chore:Atividade Spring loja de games

---
+++ haseoGames/src/main/java/com/generation/haseoGames/HaseoGamesApplication.java
@@ -0,0 +1,13 @@
+package com.generation.haseoGames;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaseoGamesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HaseoGamesApplication.class, args);
+	}
+
+}
---
+++ haseoGames/src/main/java/com/generation/haseoGames/repository/CategoriaRepository.java
@@ -0,0 +1,13 @@
+package com.generation.haseoGames.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.generation.haseoGames.model.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
+	public List<Categoria> findAllByNomeContainingIgnoreCase (String nome);
+}
